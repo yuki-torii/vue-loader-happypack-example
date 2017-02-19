eafebae8754b4b084b2e25b2cb206c2023272422
limichange <limichange@hotmail.com>
2017-02-19 15:02:24 +0800

    🚧   - No style loader is ok

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -2,6 +2,7 @@ var path = require('path')
 var utils = require('./utils')
 var config = require('../config')
 var vueLoaderConfig = require('./vue-loader.conf')
+var HappyPack = require('happypack')
 
 function resolve (dir) {
   return path.join(__dirname, '..', dir)
@@ -31,11 +32,23 @@ module.exports = {
       'components': resolve('src/components')
     }
   },
+  plugins: [
+    new HappyPack({
+      id: 'vue-loader',
+      threads: 4,
+      loaders: [ 'vue-loader' ]
+    }),
+    new HappyPack({
+      loaders: [{
+        path: 'vue-loader'
+      }]
+    })
+  ],
   module: {
     rules: [
       {
         test: /\.vue$/,
-        loader: 'vue-loader',
+        loader: 'happypack/loader?id=vue-loader',
         options: vueLoaderConfig
       },
       {
--- src/App.vue
+++ src/App.vue
@@ -16,7 +16,7 @@ export default {
 }
 </script>
 
-<style>
+<style lang="css" scoped>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
